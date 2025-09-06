80d6cc9d840d9346e16dbb314988d17d4b449568
Abid Famasya <abid.famasya@gmail.com>
2025-09-06 11:09:00 +0700

    Truncation has been re-added to 50 characters to m

--- interaction-handlers.go
+++ interaction-handlers.go
@@ -252,6 +252,10 @@ func handleCommitCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if part.Type == "text" && part.Text != "" {
 			summary = part.Text
 			slog.Debug("found AI summary in text part", "thread_id", threadID, "part_index", i, "raw_summary", summary, "length", len(summary))
+			if len(summary) > 50 {
+				summary = summary[:50]
+				slog.Debug("summary truncated to 50 chars", "thread_id", threadID, "truncated_summary", summary)
+			}
 			break // Use the first text-type part we find
 		}
 	}
